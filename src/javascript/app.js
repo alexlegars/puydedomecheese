f03fe4eb235f39afeb26224afa3ee57bc7f7d420
alexlegars <alexlegars@gmail.com>
2018-10-23 01:24:34 +0200

    anchor

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -4,8 +4,6 @@ import app from './../scss/app.scss'
 import Slider from './components/slider'
 import HoverCta from './components/hoverCta'
 import 'gsap'
-
-
 const sliders = [...document.querySelectorAll('.slider_container')]
 sliders.forEach( (item) => {
   let slider = new Slider(item)
@@ -16,3 +14,39 @@ const ctas = [...document.querySelectorAll('.cta-round')]
 ctas.forEach( (item) => {
     let cta = new HoverCta(item)
 })
+
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
