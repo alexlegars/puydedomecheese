d228fcc9b110174edb119f78484a0025d01d162c
alexlegars <alexlegars@gmail.com>
2018-10-23 02:24:33 +0200

    finish

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,19 +1,8 @@
 console.log('😎')
 
 import app from './../scss/app.scss'
-import Slider from './components/slider'
-import HoverCta from './components/hoverCta'
-import 'gsap'
-const sliders = [...document.querySelectorAll('.slider_container')]
-sliders.forEach( (item) => {
-  let slider = new Slider(item)
-})
 
 
-const ctas = [...document.querySelectorAll('.cta-round')]
-ctas.forEach( (item) => {
-    let cta = new HoverCta(item)
-})
 
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
